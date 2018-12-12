9b96f36862d46d2c6bf3f9dafcb6c4e1507588db
GuilhermeHenrique_SMN <guilherme.henrique@smn.com.br>
2018-12-12 08:25:26 -0200

    Adicionando icones e evento tap

--- lib/main.dart
+++ lib/main.dart
@@ -7,17 +7,10 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp( // Retorna um app com md
-      title: 'Welcome to the Jungle',
-      home: Scaffold( // Cria um container, com a bar e um body
-        appBar: AppBar(
-          title: Text('Flutter tutorial'),
-        ),
-        body: Center (
-            child: RandomWords()
-        ),
-      )
-    );
+    return MaterialApp(
+        // Retorna um app com md
+        title: 'Welcome to the Jungle',
+        home: RandomWords());
   }
 }
 
@@ -25,25 +18,54 @@ class RandomWordState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
+  // Guarda os nomes que foram favoritados, Set<>() não permite que sejam repetidas
+  final Set<WordPair> _saved = new Set<WordPair>();
+
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
   }
 
   Widget _buildSuggestions() {
     return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, i) {
-        if(i.isOdd) return Divider();
-        final index = i ~/ 2;
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (context, i) {
+          if (i.isOdd) return Divider();
+          final index = i ~/ 2;
 
-        if(index >= _suggestions.length) {
-          _suggestions.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_suggestions[index]);
-      }
-    );
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
+
+    return ListTile(
+        title: Text(
+          pair.asPascalCase,
+          style: _biggerFont,
+        ),
+        // Adiciona um "container" ao final, onde é colocado o icon
+        trailing: new Icon(
+            alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red : null),
+        onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              _saved.remove(pair);
+            } else {
+              _saved.add(pair);
+            }
+          });
+        });
   }
 }
 
